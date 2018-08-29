6e5532d88675883e4f2ff1518684adbae8400f54
Stef Kors <stef.kors@gmail.com>
2018-08-29 14:18:31 +0200

    fixes

--- gulpfile.js
+++ gulpfile.js
@@ -4,13 +4,14 @@ var browserSync = require("browser-sync").create();
 
 gulp.task("serve", function() {
 	browserSync.init({
-		server: "./public",
+		injectChanges: true,
+		server: "./",
 		port: 8080
 	});
 
-	gulp.watch("*.html").on("change", browserSync.reload);
-	gulp.watch("*.css").on("change", browserSync.reload);
-	gulp.watch("*.js").on("change", browserSync.reload);
+	gulp.watch("**/*.html").on("change", browserSync.reload);
+	gulp.watch("css/**/*.css").on("change", browserSync.reload);
+	gulp.watch("js/**/*.js").on("change", browserSync.reload);
 });
 
 gulp.task("default", ["serve"]);
